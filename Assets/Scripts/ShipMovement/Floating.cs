31e5e24d610dbe4473259b2204bbe9178fd5c2d1
Luis Keck <34217915+keckluis@users.noreply.github.com>
2022-10-17 15:45:11 +0200

    Revert "Revert "new xr rig""
    This reverts commit 3be1c911e7dee5b0dfc83d01ae1245b4be0abd07.

--- Assets/Scripts/ShipMovement/Floating.cs
+++ Assets/Scripts/ShipMovement/Floating.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class Floating : MonoBehaviour
 {
-    private Rigidbody Ship;
-    public float DepthBeforeSubmerged = 1;
-    public float DisplacementAmount = 3;
+    //private Rigidbody Ship;
+    //public float DepthBeforeSubmerged = 1;
+    //public float DisplacementAmount = 3;
 
     private void Start()
     {
-        Ship = GetComponent<Rigidbody>();   
+        // Ship = GetComponent<Rigidbody>();   
     }
 
     void FixedUpdate()
     {
-        if (transform.position.y < 0.5f)
+       /* if (transform.position.y < 0.5f)
         {
             float displacementMultiplier = Mathf.Clamp01(-transform.position.y / DepthBeforeSubmerged) * DisplacementAmount;
             Ship.AddForce(new Vector3(0, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0), ForceMode.Acceleration);
-        }           
+        }*/        
+
+        float y = Mathf.PingPong(Time.time * 0.25f, 0.2f);
+        transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
     }
 }
